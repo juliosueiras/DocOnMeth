71a63a644410c0de2f8a572e614d045140a8993d
Julio Tain Sueiras <juliosueiras@gmail.com>
2016-12-12 18:03:25 -0500

    Switch from fragment to list

--- doconmeth/src/main/java/com/juliosueiras/doconmeth/MainActivity.java
+++ doconmeth/src/main/java/com/juliosueiras/doconmeth/MainActivity.java
@@ -1,39 +1,26 @@
 package com.juliosueiras.doconmeth;
 
 import android.os.Bundle;
-import android.content.Context;
-import android.widget.Toast;
-import android.app.ActionBar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-import com.crashlytics.android.answers.Answers;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
-import com.facebook.stetho.Stetho;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import com.juliosueiras.doconmeth.fragments.OneFragment;
-
-
-public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
-    AppSectionsPagerAdapter mAppSectionsPagerAdapter;
-
-    ViewPager mViewPager;
+public class MainActivity extends AppCompatActivity {
+    String[] docList = {"Handlebars","IPhone","WindowsMobile","Blackberry",
+        "WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Answers(), new Crashlytics());
         setContentView(R.layout.activity_main);
-        Stetho.initializeWithDefaults(this);
-    }
 
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_doc, docArray);
+
+        ListView listView = (ListView) findViewById(R.id.doc_list);
+        listView.setAdapter(adapter);
+    }
 }
+
